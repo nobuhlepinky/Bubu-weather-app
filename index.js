cc2c26dba5d586d8df1452ee8c60edd9befa66dd
Nobuhle Pinky Nsibanyoni <nobuhlepinky11@gmail.com>
2025-10-25 07:51:02 +0200

    Added the search functionality using API Integration

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+function searchCity(city) {
+  let apiKey = "3daotdb8ad0fef1b452b37bd25c25547";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = cityInputElement.value;
+
+  searchCity(cityInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+
+searchCity("Cape Town");
